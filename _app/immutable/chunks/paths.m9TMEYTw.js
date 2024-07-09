9e71eebc65fc7eff14a2ec863a9b741cf0b795d3
rmcassio <cassio_r.m@hotmail.com>
2024-07-09 15:19:59 -0300

    Updates

---
+++ _app/immutable/chunks/paths.m9TMEYTw.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1t5gpst)==null?void 0:s.base)??"/doqi-website";var t;const a=((t=globalThis.__sveltekit_1t5gpst)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ _app/immutable/entry/start.8BDp97-k.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BqDK8tQH.js";export{t as start};
